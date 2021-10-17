35be0582feed2f858dd9eb909432e58f7358ea78
Frank Albenesius <frankalbenesius@gmail.com>
2021-10-16 20:36:38 -0500

    add da state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
 export function App() {
-  return <h1>Every Beef!</h1>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Every Beef!</h1>
+      <button type="button" onClick={() => setCount((c) => c + 1)}>
+        Clicks: {count}
+      </button>
+    </div>
+  );
 }
