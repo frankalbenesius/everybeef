7664dc870f8af8bdd9f934ee6c8215d33ea41b7d
Frank Albenesius <frankalbenesius@gmail.com>
2021-10-16 21:51:56 -0500

    Add im feelin luckeh button

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,22 +1,51 @@
-import { Box, Heading, Wrap, WrapItem } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Wrap,
+  WrapItem,
+  Stack,
+  Button,
+  Center,
+} from "@chakra-ui/react";
 import React from "react";
+import { useHistory } from "react-router";
 import { BeefSquare } from "../components/BeefSquare";
 import { getAllBeefs } from "../util/getAllBeefs";
 
+const allTheBeefs = getAllBeefs();
+
 export const IndexPage = () => {
-  const beefs = getAllBeefs();
   return (
     <Box>
       <Heading textAlign="center" py={4}>
         every beef
       </Heading>
+      <Center mb={4}>
+        <ImFeelingLuckyButton />
+      </Center>
       <Wrap spacing={1} justify="center">
-        {beefs.map((beef) => (
+        {allTheBeefs.map((beef) => (
           <WrapItem key={beef}>
             <BeefSquare id={beef} />
           </WrapItem>
         ))}
       </Wrap>
     </Box>
+  );
+};
+
+const ImFeelingLuckyButton = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    const idx = Math.floor(Math.random() * allTheBeefs.length);
+    const randomBeef = allTheBeefs[idx];
+    history.push(`/beefs/${randomBeef}`);
+  };
+
+  return (
+    <Button onClick={handleClick} colorScheme="purple">
+      I'm Feeling Lucky
+    </Button>
   );
 };
