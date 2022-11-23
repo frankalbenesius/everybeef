53d0aa9e202790cf418f76e702d67f03f44293c0
Frank Albenesius <frankalbenesius@gmail.com>
2022-11-22 18:43:56 -0600

    Use dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
   plugins: [
     react(),
     checker({
